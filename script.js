b56cf953499a001bf6cbc9eb77f58975b4d5db6b
Pavel <mcredpower@gmail.com>
2018-03-14 15:42:59 +0300

    small fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var name_array = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"] ;
 var folder = "slides";
 var current = 0;
-var path = " "
+var path = " ";
 var next_slide = document.getElementById('next_slide');
 var prew_slide = document.getElementById('prew_slide');
 var target = document.images.namedItem("target");
@@ -29,7 +29,7 @@ function path_calc () {
   check_array_range();
   path_calc();
   $('#target').fadeOut(1500);
-  setTimeout (() => {  target.src = path; },1500); 
+  setTimeout (() => {  target.src = path; },1500);
   $('#target').fadeIn(1500);
 }
 
